131d7d3542c72776153291aa42f1b87b366819ca
elsaaeid <saidsadaoy@gmail.com>
2024-01-21 04:05:34 -0800

    a

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,31 +4,25 @@
  * bubble_sort - Calls function
  * @array: Array to be sorted
  * @size: Size of array
- * Description: A function that sorts an array of integers
- * in ascending order using the Bubble sort algorithm
+ * Description: Function that sorts an array using the bubble sort method
  * Return: 0
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t x = 0, y = 0;
-	int temp;
+	unsigned int x, y;
 
-	if (array == NULL)
+	if (!array)
 		return;
 
-	while (x < size - 1)
+	for (x = 0; x < size - 1; x++)
 	{
-		while (y < size - x - 1)
+		for (y = 0; y < size - x - 1; y++)
 		{
 			if (array[y] > array[y + 1])
 			{
-				temp = array[y];
-				array[y] = array[y + 1];
-				array[y + 1] = temp;
+				swap(&array[y], &array[y + 1]);
 				print_array(array, size);
 			}
-			y++;
 		}
-		x++;
 	}
 }
