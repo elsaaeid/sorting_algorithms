b011a19920e05363a83c8ad7bdd3d28a5b70642d
Ahmedihab09 <ahmed.ihab353@gmail.com>
2024-01-21 18:44:10 -0800

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,44 @@
 #include "sort.h"
 
 /**
- * Swap two elements in an array.
- * @param xp Pointer to first element.
- * @param yp Pointer to second element.
+ * swap - Swap two elements in an array
+ * @xp: Pointer to first element
+ * @yp: Pointer to second element
  */
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
 
 /**
- * Selection sort algorithm.
- * @param array Pointer to the array to be sorted.
- * @param size Size of the array.
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-          if (array[j] < array[min_idx])
-            min_idx = j;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
 
-        if (min_idx != i)
-        {
-            swap(&array[min_idx], &array[i]);
-            print_array(array, size);
-        }
-    }
+		
+		if (min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
+
