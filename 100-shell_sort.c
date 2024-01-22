9638314c30df96f1bdca5c316254ceb15e7d44d4
elsaaeid <saidsadaoy@gmail.com>
2024-01-21 17:47:38 -0800

    a

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+* shell_sort - Sorts array using Knuth Sequence of Shell sort
+* @array: Array to be sorted
+* @size: Size of array to sort
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	size_t i, h = 1, k;
+	int tmp;
+
+	if (!array)
+		return;
+	while (h < size)
+		h = 3 * h + 1;
+	if (h >= size)
+		h = (h - 1) / 3;
+	while (h > 0)
+	{
+		for (i = h; i < size; i++)
+		{
+			tmp = array[i];
+			for (k = i; (k > h - 1) && (tmp < array[k - h]); k -= h)
+			{
+				array[k] = array[k - h];
+			}
+			array[k] = tmp;
+		}
+		h = (h - 1) / 3;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -45,4 +45,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 
+/* Prototypes for advanced tasks */
+void shell_sort(int *array, size_t size);
+
+
 #endif /* SORT_H */
